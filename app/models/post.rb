00ba4a94d9a9ffbf7c2bc93cbf4811b8ea05c2a9
Trevor Strieber <trevor@strieber.org>
2013-08-24 22:34:10 -0400

    Running annotate.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  body       :text
+#  user_id    :integer
+#  points     :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Post < ActiveRecord::Base
 end
