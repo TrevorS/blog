6a52dc82bf5a770544bb4c8abb0c5b42c83b2401
Trevor Strieber <trevor@strieber.org>
2013-08-24 22:18:55 -0400

    Running migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130825020517) do
+ActiveRecord::Schema.define(version: 20130825021309) do
 
   create_table "posts", force: true do |t|
     t.string   "name"
